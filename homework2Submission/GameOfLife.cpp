43062057afcc377bf520cd3c99923a393214aaec
Marcelo Torre <mtorres3@crimson.au.edu>
2024-09-25 22:43:43 -0500

    Added hw 2

--- homework2Submission/GameOfLife.cpp
+++ homework2Submission/GameOfLife.cpp
@@ -3,8 +3,8 @@ Name: Marcelo Torres
 Email: mtorres3@crimson.ua.edu
 Course Section: CS 481
 Homework #: 1
-Instructions to compile the program: (for example: gcc -Wall -O -o hw1 hw1.c)
-Instructions to run the program: (for example: ./hw1 <size> <iterations>)
+Instructions to compile the program: ./build.sh
+Instructions to run the program: ./Homework1 <board size> <iterations>
 */
 
 #include <iostream>
@@ -23,21 +23,22 @@ using namespace std;
 using namespace util;
 
 #define DEBUG_LOGGING
+// #define CELL_UPDATE_DEBUG_LOGGING
+
+
 
 int main(int argc, char** argv) {
 
-    int rows = 1000;
+    int rows = 5000;
     int columns = rows;
 
     int iterations = 5000;
-
-    constexpr bool useInitializerList = false;
 
     constexpr int maxOffset = 1;
 
     constexpr int printThreshold = 50;
 
-    int numThreads = 10;
+    constexpr int numThreads = 10;
 
     if (argc < 3) {
         cout << "Using coded constants" << endl;
@@ -53,13 +54,6 @@ int main(int argc, char** argv) {
         rows = atoi(argv[1]);
         columns = atoi(argv[2]);
         iterations = atoi(argv[3]);
-    }
-    else if (argc == 5) {
-        cout << "Using rows: " << argv[1] << " and columns: " << argv[2] << " and iterations: " << argv[3] << " and numThreads: " << argv[4] << endl;
-        rows = atoi(argv[1]);
-        columns = atoi(argv[2]);
-        iterations = atoi(argv[3]);
-        numThreads = atoi(argv[4]);
     }
 
     int printCount = max(iterations / 10, 1);
@@ -78,32 +72,13 @@ int main(int argc, char** argv) {
     cout << "fill with random complete" << endl;
     #endif
 
-    vector<bool> initializer = {
-            true, true, false,
-            true, true, false,
-            false, false, false
-    };
+     vector<bool> initializer = {
+         true, true, false,
+         true, true, false,
+         false, false, false
+     };
 
-    vector<bool> initializer2 = {
-            true, true, false, false, true,
-            false, true, true, false, false,
-            true, true, false, false, true,
-            false, true, true, true, true,
-            true, true, true, false, true
-    };
-
-    vector<bool> test2 = {
-        true, true, true, false, false,
-        false, false, true, true, false,
-        true, false, false, false, true,
-        false, false, false, false, true,
-        true, false, false, false, true,
-    };
-
-    if (useInitializerList) {
-//        matrix.fillFromVector(initializer);
-        matrix.fillFromVector(initializer2);
-    }
+    // matrix.setFromVector(initializer);
 
     const int sum = matrix.getSum();
 
@@ -122,22 +97,16 @@ int main(int argc, char** argv) {
 
     start = chrono::system_clock::now();
 
-    bool updateOccurred;
-
-    auto groups = calculateRowGroups(matrix, numThreads);
-
     for (int i = 0; i < iterations; i++) {
 
-//        updateOccurred = updateCells(matrix);
+//        updateCells(matrix);
 
-//        updateOccurred = updateCellsUsingThreadPool(matrix, threadPool, groups);
+        bool updateOccurred = updateCellsUsingThreadPool(matrix, threadPool, numThreads);
 
-        updateOccurred = updateCellsUsingThreadPool_Windows(matrix, threadPool, groups);
-
-//        if (i == printCount * multiplier) {
-//            cout << "On iteration: " << i << " , " << (i/static_cast<double>(iterations))*100.0  << "%" << endl;
-//              multiplier++;
-//        }
+        if (i == printCount * multiplier) {
+            cout << "On iteration: " << i << " , " << (i/static_cast<double>(iterations))*100.0  << "%" << endl;
+              multiplier++;
+        }
 
         if (!updateOccurred) {
             cout << "exiting early because no update" << endl;
@@ -155,23 +124,6 @@ int main(int argc, char** argv) {
     if (rows*columns < printThreshold*printThreshold) {
         cout << "end matrix " << endl;
         cout << matrix << endl;
-    }
-
-    if (useInitializerList) {
-        if (matrix.rows() * matrix.columns() < printThreshold * printThreshold) {
-            bool success = true;
-
-            for (int i = 0; i < matrix.rows(); i++) {
-                for (int j = 0; j < matrix.columns(); j++) {
-                    if (test2.at((i * rows) + j) != matrix.get(i, j)) {
-                        success = false;
-                        break;
-                    }
-                }
-            }
-
-            cout << "Success2: " << boolalpha << success << endl;
-        }
     }
 
     const double percent = (sum / static_cast<double>(rows*columns)) * 100.0;
