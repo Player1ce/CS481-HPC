e60eb8134dceb74533efd41d79400e42eafb8162
Marcelo Torres <mtorres3@crimson.ua.edu>
2024-09-26 04:07:04 -0500

    Moved results to folder and added/updated some tests

---
+++ homework2Submission/test_10000-5000.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load intel
+
+# calculated time: 5.2 hours per run
+
+# ~size: 200MB +- some
+
+# DO NOT RUN THIS!
+
+# params:
+# cores: 1
+# mem: 1000Mb
+# time: 32:00:00
+
+./Homework2 10000 5000
+./Homework2 10000 5000
+./Homework2 10000 5000
