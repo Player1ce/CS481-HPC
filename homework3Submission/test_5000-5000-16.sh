b228b934dca5c341137c9a09bbef8725880c9bf4
Marcelo Torre <mtorres3@crimson.au.edu>
2024-10-13 17:30:33 -0500

    Added test script and moved results

---
+++ homework3Submission/test_5000-5000-16.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load intel
+
+
+numThreads=16;
+directory='/scratch/ualclsd0201/'
+test_file='output_5000x5000_5000_0.txt'
+
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
