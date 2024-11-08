b67c88e299cfedd754cdd39e0b2352b931a6300f
Marcelo Torres <mtorres3@crimson.ua.edu>
2024-11-08 15:11:01 -0600

    Updated hw3 test

---
+++ homework4Submission/test_hw3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+module load intel
+
+#numThreads=1;
+directory='/scratch/ualclsd0201/'
+test_file='output_5000x5000_5000_0.txt'
+
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#
+#numThreads=2;
+#
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#
+#numThreads=4;
+#
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+#
+numThreads=8;
+
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+
+numThreads=10;
+
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+
+numThreads=16;
+
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+
+numThreads=20;
+
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+./Homework3 5000 5000 "$numThreads" "$directory" "$test_file"
+
+for fileNum in {1..21}
+do
+  diff output_5000x5000_5000_"$fileNum".txt test_file
+done
