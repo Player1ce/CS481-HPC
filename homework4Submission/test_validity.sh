a7fb177fa2c48a637b0196b6cb0deabe705bc136
Marcelo Torres <mtorres3@crimson.ua.edu>
2024-11-08 02:42:16 -0600

    Updated test_validity.sh to fix bug

--- homework4Submission/test_validity.sh
+++ homework4Submission/test_validity.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-module load intel
+source /apps/profiles/modules_asax.sh.dyn
+
 module  load  openmpi/4.1.4-gcc11
+module load intel
 
 directory='/scratch/ualclsd0201/'
 #directory='validation/'
