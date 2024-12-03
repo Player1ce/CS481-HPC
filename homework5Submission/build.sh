772ab464bbf6fa4477a36b0e3b179245f9835809
Marcelo Torres <mtorres3@crimson.ua.edu>
2024-12-03 09:57:26 -0600

    updated cmake

--- homework5Submission/build.sh
+++ homework5Submission/build.sh
@@ -3,10 +3,10 @@
 source /apps/profiles/modules_asax.sh.dyn
 
 #module  load  openmpi/4.1.4-gcc11
-#module load intel
+module load intel
 
-module load cuda
 module load cmake
+module load cuda
 
 cmake . -DCMAKE_BUILD_TYPE=Release  -B auto-build
 
